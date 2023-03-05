fc9c02eefc3abfaf8809df0bad713d1d4be7b058
asyl <asyl.kydraliev@gmail.com>
2023-03-05 23:16:30 +0600

    feat: Create add form and drag-n-drop list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="col-5">
+      <div class="d-flex gap-2">
+        <b-form-input
+            v-model="newTask"
+            placeholder="New task"
+            @keyup.enter="add"
+        />
+        <b-button variant="primary" class="ml-3 col-2" @click="add">Add</b-button>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>Todos</h3>
+          <draggable class="list-group kanban-column" :list="todos" group="todos">
+            <div class="list-group-item" v-for="todo in todos" :key="todo.name">
+              {{ todo.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-primary">
+          <h3>In progress</h3>
+          <draggable class="list-group kanban-column" :list="inProgress" group="todos">
+            <div class="list-group-item" v-for="todo in inProgress" :key="todo.name">
+              {{ todo.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
+          <h3>On tested</h3>
+          <draggable class="list-group kanban-column" :list="onTested" group="todos">
+            <div class="list-group-item" v-for="todo in onTested" :key="todo.name">
+              {{ todo.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <draggable class="list-group kanban-column" :list="done" group="todos">
+            <div class="list-group-item" v-for="todo in done" :key="todo.name">
+              {{ todo.name }}
+            </div>
+          </draggable>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { VueDraggableNext } from "vue-draggable-next";
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    draggable: VueDraggableNext
+  },
+
+  data() {
+    return {
+      newTask: '',
+      todos: [
+        {name: "Code Sign Up Page"},
+        {name: "Test dashboard"},
+        {name: "Style registration"},
+        {name: "Help with design"}
+      ],
+      inProgress: [],
+      onTested: [],
+      done: [],
+    }
+  },
+
+  methods: {
+    add() {
+      if (this.newTask) {
+        this.todos.push({name: this.newTask});
+        this.newTask = "";
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.kanban-column {
+  min-height: 500px;
 }
 </style>
